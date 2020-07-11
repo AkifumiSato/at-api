8fa00f925fdbce8996d178754df2999c53ce01b3
佐藤昭文 <akfm.sato@gmail.com>
2020-07-11 19:38:06 +0900

    post request add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    middleware, web, App, HttpResponse, HttpServer,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct MyObj {
+    name: String,
+    number: i32,
+}
+
+async fn index(item: web::Json<MyObj>) -> HttpResponse {
+    println!("model: {:?}", &item);
+    HttpResponse::Ok().json(item.0) // <- send response
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
+            .service(web::resource("/").route(web::post().to(index)))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::dev::Service;
+    use actix_web::{http, test, Error, web, App};
+
+    #[actix_rt::test]
+    async fn test_index() -> Result<(), Error> {
+        let mut app = test::init_service(
+            App::new().service(web::resource("/").route(web::post().to(index))),
+        )
+            .await;
+
+        let req = test::TestRequest::post()
+            .uri("/")
+            .set_json(&MyObj {
+                name: "my-name".to_owned(),
+                number: 43,
+            })
+            .to_request();
+        let resp = app.call(req).await.unwrap();
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = match resp.response().body().as_ref() {
+            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+            _ => panic!("Response error"),
+        };
+
+        assert_eq!(response_body, r##"{"name":"my-name","number":43}"##);
+
+        Ok(())
+    }
 }
