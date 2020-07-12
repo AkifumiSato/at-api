cc2bdca650af464d3ad431cb3657726c536fccec
佐藤昭文 <akfm.sato@gmail.com>
2020-07-12 21:51:42 +0900

    refactor create_post.rs

--- src/db.rs
+++ src/db.rs
@@ -4,6 +4,15 @@ use dotenv::dotenv;
 use std::env;
 use super::schema::posts;
 
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
 #[derive(Insertable)]
 #[table_name="posts"]
 pub struct NewPost<'a> {
@@ -19,11 +28,8 @@ pub struct Post {
     pub published: bool,
 }
 
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url))
+pub fn create_post<'a>(connection: &PgConnection, post: NewPost) -> Result<Post, diesel::result::Error> {
+    diesel::insert_into(posts::table)
+        .values(post)
+        .get_result::<Post>(connection)
 }
--- src/routes/create_post.rs
+++ src/routes/create_post.rs
@@ -1,9 +1,6 @@
+use crate::db::{NewPost, establish_connection, create_post};
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
-use diesel::prelude::*;
-use crate::schema::posts;
-use crate::db::{Post, NewPost, establish_connection};
-use crate::diesel;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct PostJson {
@@ -19,10 +16,8 @@ pub async fn index(item: web::Json<PostJson>) -> HttpResponse {
         body: &item.body,
     };
 
-    match diesel::insert_into(posts::table)
-        .values(&new_post)
-        .get_result::<Post>(&connection) {
+    match create_post(&connection, new_post) {
         Ok(post) => HttpResponse::Ok().body(format!("post title is {}", post.title)),
-        Err(_e) => HttpResponse::Ok().body("Error saving new post"),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
     }
 }
