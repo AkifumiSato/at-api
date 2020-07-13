c270b9fa2a035285b90b69434be273b8dd0c6d21
佐藤昭文 <akfm.sato@gmail.com>
2020-07-14 01:30:53 +0900

    scenario test add

--- src/db.rs
+++ src/db.rs
@@ -43,6 +43,7 @@ pub fn show_post<'a>(connection: &PgConnection, count: i64, page: i64) -> Result
     posts.filter(published.eq(true))
         .limit(count)
         .offset(offset)
+        .order(id.desc())
         .load::<Post>(connection)
 }
 
@@ -52,4 +53,47 @@ pub fn publish_post<'a>(connection: &PgConnection, target_id: i32) -> Result<Pos
     diesel::update(posts.find(target_id))
         .set(published.eq(true))
         .get_result::<Post>(connection)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn init() -> PgConnection {
+        let db = establish_connection();
+        db.begin_test_transaction().unwrap();
+        db
+    }
+
+    #[test]
+    fn scenario() {
+        let connection = init();
+
+        let new_post1 = NewPost {
+            title: "unit test title111",
+            body: "unit test body111",
+        };
+
+        let created_posts = create_post(&connection, new_post1).unwrap();
+        let _published_post = publish_post(&connection, created_posts.id);
+
+        let new_post2 = NewPost {
+            title: "unit test title222",
+            body: "unit test body222",
+        };
+
+        let created_posts = create_post(&connection, new_post2).unwrap();
+        let _published_post = publish_post(&connection, created_posts.id);
+
+        let posts = show_post(&connection, 2, 1).unwrap();
+
+        let result = posts
+            .iter()
+            .map(|item| {
+                item.title.clone()
+            })
+            .collect::<Vec<String>>();
+
+        assert_eq!(result, ["unit test title222", "unit test title111"]);
+    }
 }
