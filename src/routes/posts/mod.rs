8d4816b9a6f240d5ac6529a43b5ead38b50c7b7b
佐藤昭文 <akfm.sato@gmail.com>
2020-07-20 18:41:00 +0900

    test refactor

--- src/routes/posts/mod.rs
+++ src/routes/posts/mod.rs
@@ -38,10 +38,42 @@ mod tests {
     ///
     /// 1. create
     /// 2. show
-    /// 3. update
-    /// 4. delete
     #[actix_rt::test]
-    async fn test_scenario() {
+    async fn test_get_posts() {
+        let pool = setup_connection_pool();
+
+        let mut app = test::init_service(
+            App::new()
+                .data(pool.clone())
+                .data(web::JsonConfig::default().limit(4096))
+                .service(web::scope("/").configure(config))
+        ).await;
+
+        let req = test::TestRequest::post()
+            .uri("/posts/")
+            .set_json(&create::JsonBody::new("unit test title", "unit test body", Some(true)))
+            .to_request();
+        let resp: posts::Post = test::read_response_json(&mut app, req).await;
+        let id = resp.id;
+        assert_eq!("unit test title", resp.title);
+        assert_eq!("unit test body", resp.body);
+
+        let req = test::TestRequest::get()
+            .uri("/posts/?count=1")
+            .to_request();
+        let resp: show::Response = test::read_response_json(&mut app, req).await;
+        assert_eq!(1, resp.result.iter().len());
+        assert_eq!(id, resp.result.first().unwrap().id);
+    }
+
+    /// # scenario
+    ///
+    /// 1. create
+    /// 2. show
+    /// 3. update
+    /// 4. show
+    #[actix_rt::test]
+    async fn test_patch() {
         let pool = setup_connection_pool();
 
         let mut app = test::init_service(
@@ -60,6 +92,12 @@ mod tests {
         assert_eq!("unit test title", resp.title);
         assert_eq!("unit test body", resp.body);
 
+        let req = test::TestRequest::get()
+            .uri("/posts/?count=1")
+            .to_request();
+        let resp: show::Response = test::read_response_json(&mut app, req).await;
+        assert_ne!(id, resp.result.first().unwrap().id);
+
         let req = test::TestRequest::patch()
             .uri("/posts/")
             .set_json(&update::JsonBody::new(id, None, None, Some(true)))
@@ -73,29 +111,16 @@ mod tests {
         let resp: show::Response = test::read_response_json(&mut app, req).await;
         assert_eq!(1, resp.result.iter().len());
         assert_eq!(id, resp.result.first().unwrap().id);
-
-        let req = test::TestRequest::patch()
-            .uri("/posts/")
-            .set_json(&update::JsonBody::new(id, Some("update test title"), Some("update test body"), Some(true)))
-            .to_request();
-        let resp: posts::Post = test::read_response_json(&mut app, req).await;
-        assert_eq!("update test title", resp.title);
-        assert_eq!("update test body", resp.body);
-
-        let req = test::TestRequest::delete()
-            .uri("/posts/")
-            .set_json(&delete::JsonBody::new(id))
-            .to_request();
-        let resp = test::call_service(&mut app, req).await;
-        assert!(resp.status().is_success());
     }
 
-    /// # scenario2
+    /// # scenario
     ///
     /// 1. create
     /// 2. find
+    /// 3. delete
+    /// 4. find
     #[actix_rt::test]
-    async fn test_scenario2() {
+    async fn test_delete_find() {
         let pool = setup_connection_pool();
 
         let mut app = test::init_service(
@@ -121,5 +146,19 @@ mod tests {
         let post = resp.result.unwrap();
         assert_eq!("unit test title", post.title);
         assert_eq!("unit test body", post.body);
+
+        let req = test::TestRequest::delete()
+            .uri("/posts/")
+            .set_json(&delete::JsonBody::new(id))
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+
+        let req = test::TestRequest::get()
+            .uri(&format!("/posts/{}/", id))
+            .to_request();
+        let resp: find::Response = test::read_response_json(&mut app, req).await;
+        let post = resp.result;
+        assert!(post.is_none());
     }
 }
