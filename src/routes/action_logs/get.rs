8617a5b7ec375be9de9fe5e5980def3bc9ddf6cd
佐藤昭文 <akfm.sato@gmail.com>
2020-08-29 13:19:07 +0900

    action record route

--- src/routes/action_logs/get.rs
+++ src/routes/action_logs/get.rs
@@ -1,14 +1,37 @@
 use crate::database_utils::pool::DbPool;
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
+use crate::usecase::action_records::get_records::{self, InputData};
+use crate::driver::action_records::ActionRecordDriver;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GetParams {
+    pub user_id: i32,
     pub page: Option<i32>,
     pub count: Option<i32>,
 }
 
-pub async fn index(_pool: web::Data<DbPool>, _item: web::Query<GetParams>) -> HttpResponse {
-    HttpResponse::Ok().body("Hello, Action log.")
-    // todo: get_records::executeする
+impl GetParams {
+    pub fn to_input_data(&self) -> InputData {
+        let page = self.page.unwrap_or_else(|| 1);
+        let count = self.count.unwrap_or_else(|| 10);
+
+        InputData {
+            user_id: self.user_id,
+            page,
+            count,
+        }
+    }
+}
+
+pub async fn index(pool: web::Data<DbPool>, item: web::Query<GetParams>) -> HttpResponse {
+    let connection = pool
+        .get()
+        .expect("couldn't get driver connection from pool");
+    let action_driver = ActionRecordDriver::new(&connection);
+
+    match get_records::execute(action_driver, item.to_input_data()) {
+        Ok(action_records) => HttpResponse::Created().json(action_records),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
+    }
 }
